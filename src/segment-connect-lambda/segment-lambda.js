531ca3e0860921096960fffd36f24b0d25a3ae94
manbearshark <igorkrtolica@maxslaughtermbp.lan>
2023-07-22 18:31:48 -0700

    added lambda project files

---
+++ src/segment-connect-lambda/segment-lambda.js
@@ -0,0 +1,47 @@
+const { Analytics } = require('@segment/analytics-node');
+
+exports.identify = async function (event, context) {
+    const analytics = new Analytics({ writeKey: '<YOUR_WRITE_KEY>' });
+
+    // Get your parameters from the Connect invocation
+    let phone = event['Details']['ContactData']['CustomerEndpoint']['Address'];
+
+    // Maybe do a lookup to Segment API here by phone number?
+
+    // Then merge the profile from Connect back to Segment...
+    analytics.identify({
+        userId:'f4ca124298',
+        traits: {
+          name: 'Michael Bolton',
+          email: 'mbolton@example.com',
+          createdAt: new Date('2014-06-14T02:00:19.467Z')
+        }
+      });
+    
+    // Return the merged profile back to Connect flow
+    // result = {
+    //         "AccountId": customerAccountId,
+    //         "Balance": '$%s' % customerBalance
+    //     };
+    // return result;
+};
+
+exports.track = async function (event, context) {
+    const analytics = new Analytics({ writeKey: '<YOUR_WRITE_KEY>' });
+
+    console.log(JSON.stringify(event));
+
+    return {};
+    // Get your parameters from the Connect invocation
+    // let phone = event['Details']['ContactData']['CustomerEndpoint']['Address'];
+    // let eventName = event['Details']['EventName'];
+
+    // analytics.track({
+    //     event: eventName,
+    //     userId: '',
+    //     properties: {
+    //       callFlow: 'Michael Bolton',
+    //       email: 'mbolton@example.com'
+    //     }
+    //   });    
+};
